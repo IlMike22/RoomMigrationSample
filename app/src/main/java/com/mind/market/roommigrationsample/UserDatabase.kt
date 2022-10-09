d5b7b0b7b44fb1e4361faf6a35debad79e2cf072
IlMike22 <michaelwidlok@yahoo.de>
2022-10-09 21:32:38 +0200

    changed db column field using AutoMigrationSpec

--- app/src/main/java/com/mind/market/roommigrationsample/UserDatabase.kt
+++ app/src/main/java/com/mind/market/roommigrationsample/UserDatabase.kt
@@ -2,15 +2,25 @@ package com.mind.market.roommigrationsample
 
 import androidx.room.AutoMigration
 import androidx.room.Database
+import androidx.room.RenameColumn
 import androidx.room.RoomDatabase
+import androidx.room.migration.AutoMigrationSpec
 
 @Database(
     entities = [User::class],
-    version = 2,
+    version = 3,
     autoMigrations = [
-        AutoMigration(from = 1, to = 2)
+        AutoMigration(from = 1, to = 2),
+        AutoMigration(from = 2, to = 3, spec = UserDatabase.Migration2To3::class)
     ]
 )
 abstract class UserDatabase : RoomDatabase() {
     abstract val dao: UserDao
+
+    // needed if some field name was changed like "created" field
+    // use DeleteColumn or DeleteTable for those modifications on your db
+    @RenameColumn(tableName = "User", fromColumnName = "created", toColumnName = "createdAt")
+    class Migration2To3():AutoMigrationSpec
+
+
 }
